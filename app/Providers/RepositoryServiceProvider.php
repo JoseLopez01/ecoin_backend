8c472204331516c864075bb32d201308be12337c
Jose Lopez <lopezarizajosealejandro@gmail.com>
2021-05-15 18:09:25 -0500

    repositorios registrados

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -25,6 +25,62 @@ class RepositoryServiceProvider extends ServiceProvider
             'App\Interfaces\SemesterInterface',
             'App\Repositories\SemesterRepository'
         );
+        $this->app->bind(
+            'App\Interfaces\WeekdayInterface',
+            'App\Repositories\WeekDayRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\ActivityInterface',
+            'App\Repositories\ActivityRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\CourseInterface',
+            'App\Repositories\CourseRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\DeliverableFileInterface',
+            'App\Repositories\DeliverableFileRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\DeliverableInterface',
+            'App\Repositories\DeliverableRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\PriceInterface',
+            'App\Repositories\PriceRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\RewardInterface',
+            'App\Repositories\RewardRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\SaleDetailInterface',
+            'App\Repositories\SaleDetailRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\SaleHeaderInterface',
+            'App\Repositories\SaleHeaderRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\SaleStatusInterface',
+            'App\Repositories\SaleStatusRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\ShopInterface',
+            'App\Repositories\ShopRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\StudentCourseInterface',
+            'App\Repositories\StudentCourseRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\DeliverableStatusInterface',
+            'App\Repositories\DeliverableStatusRepository'
+        );
+        $this->app->bind(
+            'App\Interfaces\CourseScheduleInterface',
+            'App\Repositories\CourseScheduleRepository'
+        );
     }
 
     /**
